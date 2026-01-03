2dc31ad8b198ee69ed38b794113252f106ab83aa
GREYHAT262 <158593330+GREYHAT262@users.noreply.github.com>
2026-01-03 19:46:18 +0800

    Complete 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const processedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedStr = processedStr.split('').reverse().join('');
+    return processedStr === reversedStr;
 };
+
+palindromes('ZZZZ car, a man, a maracaz.');
 
 // Do not edit below this line
 module.exports = palindromes;
