cf98574e1d11aa66c35981959f7c19507ef99480
GREYHAT262 <158593330+GREYHAT262@users.noreply.github.com>
2026-01-03 22:51:35 +0800

    Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+    if (!person.yearOfDeath) {
+        person.yearOfDeath = new Date().getFullYear();
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    const sortByDescendingAge = people.toSorted((p1, p2) => getAge(p2) - getAge(p1));
+    return sortByDescendingAge[0];
 };
 
 // Do not edit below this line
